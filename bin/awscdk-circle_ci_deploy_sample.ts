d46b8a5e8742999747f32d6102d8dd136ab401cf
gnk263 <gnk263@users.noreply.github.com>
2019-08-18 21:04:50 +0900

    CircleCI用のユーザを作成（暫定）

--- bin/awscdk-circle_ci_deploy_sample.ts
+++ bin/awscdk-circle_ci_deploy_sample.ts
@@ -2,8 +2,11 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { AppStack } from '../lib/app-stack';
+import { CircleCIUserStack } from '../lib/circleci-user-stack';
 
 const targetEnv = process.env.SYSTEM_ENV ? process.env.SYSTEM_ENV : 'dev';
 
 const app = new cdk.App();
 new AppStack(app, `AWS-CDK-CircleCI-Deploy-Sample-${targetEnv}`, targetEnv);
+
+new CircleCIUserStack(app, `AWS-CDK-CircleCI-Deploy-Sample-User-Stack-${targetEnv}`, targetEnv);
