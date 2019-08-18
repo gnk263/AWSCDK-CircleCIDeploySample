c3f09358cf456c7842efab3b17c5ba23cd989943
gnk263 <gnk263@users.noreply.github.com>
2019-08-18 23:34:05 +0900

    add set_aws.sh

---
+++ scripts/set_aws.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/cli-configure-envvars.html
+
+export SYSTEM_ENV=$1
+export AWS_DEFAULT_REGION="ap-northeast-1"
+export AWS_DEFAULT_OUTPUT="json"
+
+
+if [ $1 = "dev" ]; then
+    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
+    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
+else
+    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PROD
+    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PROD
+fi
